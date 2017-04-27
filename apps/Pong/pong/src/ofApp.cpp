c7b51cda26f2c5ac1bbd65db77f3e1df656b9bbd
Brian Eschrich <hi@brinoausrino.com>
2017-04-27 14:39:52 +0200

    pitest

--- apps/Pong/pong/src/ofApp.cpp
+++ apps/Pong/pong/src/ofApp.cpp
@@ -136,30 +136,30 @@ void ofApp::initWarper(){
 void ofApp::drawWarpedImage(){
     //======================== get our quad warp matrix.
     
-    ofMatrix4x4 mat = warper.getMatrix();
-    
-    //======================== use the matrix to transform our fbo.
-    
-    ofPushMatrix();
-    ofMultMatrix(mat);
+    /*ofMatrix4x4 mat = warper.getMatrix();
+     
+     //======================== use the matrix to transform our fbo.
+     
+     ofPushMatrix();
+     ofMultMatrix(mat);*/
     gameFbo.draw(0, 0);
-    ofPopMatrix();
-    
-    //======================== draw quad warp ui.
-    
-    ofSetColor(ofColor::magenta);
-    warper.drawQuadOutline();
-    
-    ofSetColor(ofColor::yellow);
-    warper.drawCorners();
-    
-    ofSetColor(ofColor::magenta);
-    warper.drawHighlightedCorner();
-    
-    ofSetColor(ofColor::red);
-    warper.drawSelectedCorner();
-    
-    ofSetColor(ofColor::white);
+    /*ofPopMatrix();
+     
+     //======================== draw quad warp ui.
+     
+     ofSetColor(ofColor::magenta);
+     warper.drawQuadOutline();
+     
+     ofSetColor(ofColor::yellow);
+     warper.drawCorners();
+     
+     ofSetColor(ofColor::magenta);
+     warper.drawHighlightedCorner();
+     
+     ofSetColor(ofColor::red);
+     warper.drawSelectedCorner();
+     
+     ofSetColor(ofColor::white);*/
 }
 
 /**
